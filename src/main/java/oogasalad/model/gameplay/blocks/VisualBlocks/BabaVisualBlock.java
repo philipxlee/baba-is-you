76ef238674ad2612660e117aaf8ff8e6816c2f84
Philip Lee <philipleektx@gmail.com>
2024-03-27 22:06:21 -0400

    [CREATE-BABA-VISUAL-BLOCK]: Created visual block for Baba with behaviors

--- src/main/java/oogasalad/model/gameplay/blocks/VisualBlocks/BabaVisualBlock.java
+++ src/main/java/oogasalad/model/gameplay/blocks/VisualBlocks/BabaVisualBlock.java
@@ -1,2 +1,18 @@
-package oogasalad.model.gameplay.blocks.VisualBlocks;public class BabaVisualBlock {
+package oogasalad.model.gameplay.blocks.VisualBlocks;
+
+import oogasalad.model.gameplay.strategies.Controllable;
+import oogasalad.model.gameplay.strategies.Pushable;
+
+public class BabaVisualBlock extends AbstractVisualBlock {
+
+  public BabaVisualBlock(String name, String imagePath) {
+    super(name, imagePath);
+  }
+
+  @Override
+  public void initializeBehaviors() {
+    addBehavior(new Controllable());
+    addBehavior(new Pushable());
+  }
+
 }
