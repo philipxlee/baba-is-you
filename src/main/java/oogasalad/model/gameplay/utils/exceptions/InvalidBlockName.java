167860efdc7518344fe89b0fc6cae22b530722d8
Philip Lee <philipleektx@gmail.com>
2024-03-30 14:17:26 -0400

    [DESIGN]: Added docstrings to fix pipeline for model exceptions util

--- src/main/java/oogasalad/model/gameplay/utils/exceptions/InvalidBlockName.java
+++ src/main/java/oogasalad/model/gameplay/utils/exceptions/InvalidBlockName.java
@@ -1,14 +1,14 @@
 package oogasalad.model.gameplay.utils.exceptions;
 
 /**
- * Exception thrown when the block name is invalid
+ * Exception thrown when the block name is invalid.
  */
 public class InvalidBlockName extends Exception {
 
   /**
-   * Throws an exception when the block name is invalid
+   * Throws an exception when the block name is invalid.
    *
-   * @param blockName the name of the block
+   * @param blockName the name of the block.
    */
   public InvalidBlockName(String blockName) {
     super("Invalid Block Name " + blockName + ". Check the block name in the JSON file.");
--- src/main/java/oogasalad/model/gameplay/utils/exceptions/VisitorReflectionException.java
+++ src/main/java/oogasalad/model/gameplay/utils/exceptions/VisitorReflectionException.java
@@ -1,7 +1,16 @@
 package oogasalad.model.gameplay.utils.exceptions;
 
+/**
+ * Exception thrown when the reflection for the visitor fails.
+ */
 public class VisitorReflectionException extends Exception {
 
+  /**
+   * Throws an exception when the reflection for the visitor fails.
+   *
+   * @param visitorName the name of the visitor.
+   * @param cause       the cause of the exception.
+   */
   public VisitorReflectionException(String visitorName, Throwable cause) {
     super("Could not instantiate a Visitor " +
           "Possible error in reflection for visitor " + visitorName, cause);
