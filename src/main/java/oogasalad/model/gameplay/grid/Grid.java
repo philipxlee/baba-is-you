c7710991f68325aba0d35bde41086b4c2a05e051
Joseph Ogunbadewa <ojo4@duke.edu>
2024-03-31 16:49:02 -0400

    made cells one element instead of two

--- src/main/java/oogasalad/model/gameplay/grid/Grid.java
+++ src/main/java/oogasalad/model/gameplay/grid/Grid.java
@@ -48,14 +48,15 @@ public class Grid {
     }
   }
   String[][][] tempconfiguration = {
-          {{"BabaVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"RockVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"EmptyVisualBlock", "BabaVisualBlock"}, {"RockTextBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"RockVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"BabaTextBlock", "EmptyVisualBlock"}, {"IsTextBlock", "EmptyVisualBlock"}, {"YouTextBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"RockVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}},
-          {{"BabaVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}, {"EmptyVisualBlock", "EmptyVisualBlock"}}
+          {{"BabaVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}},
+          {{"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"RockVisualBlock"}, {"EmptyVisualBlock"}},
+          {{"EmptyVisualBlock"}, {"RockTextBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}},
+          {{"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}},
+          {{"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"RockVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}},
+          {{"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"BabaTextBlock"}, {"IsTextBlock"}, {"YouTextBlock"}, {"EmptyVisualBlock"}},
+          {{"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"RockVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}},
+          {{"BabaVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}, {"EmptyVisualBlock"}}
   };
+
 
 }
