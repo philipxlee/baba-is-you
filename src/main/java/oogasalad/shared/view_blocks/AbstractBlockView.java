91e958672a7da4e81ccecdb9c26fcbce4d39673c
Jonathan Esponda <je161@duke.edu>
2024-03-31 10:35:14 -0400

    [VIEW BLOCK CLASSES] Created abstract blockview class

---
+++ src/main/java/oogasalad/shared/view_blocks/AbstractBlockView.java
@@ -0,0 +1,33 @@
+package oogasalad.shared.view_blocks;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+/**
+ * Represents a generic view block within the game. This class provides the base functionality and
+ * interface for the view blocks and will be shared between the game player and authoring
+ * environment.
+ */
+public abstract class AbstractBlockView {
+  private StackPane stackPane;
+
+  /**
+   * This method initializes the view block by going to the image path and making it the image
+   * for the imageView.
+   *
+   * @param imgPath String that holds the path to the block image.
+   */
+  public AbstractBlockView initializeBlock(String imgPath) {
+    ImageView imageView = new ImageView(imgPath);
+    stackPane = new StackPane(imageView);
+    return this;
+  }
+
+  /**
+   * The getView() method returns the stackPane that represents the view of the block.
+   */
+  public StackPane getView() {
+    return this.stackPane;
+  }
+
+}
