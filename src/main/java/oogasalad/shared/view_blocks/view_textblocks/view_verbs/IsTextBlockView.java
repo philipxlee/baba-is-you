aceb3ecc7e91565cde875d0ad2d6fc6071b6bde6
Jonathan Esponda <je161@duke.edu>
2024-03-31 11:04:17 -0400

    [VIEW BLOCK CLASSES] created view_verbs and added a class for the text block view of the verb is

---
+++ src/main/java/oogasalad/shared/view_blocks/view_textblocks/view_verbs/IsTextBlockView.java
@@ -0,0 +1,18 @@
+package oogasalad.shared.view_blocks.view_textblocks.view_verbs;
+
+import oogasalad.shared.view_blocks.AbstractBlockView;
+
+/**
+ * A view for the text block that represents the "is" verb.
+ */
+public class IsTextBlockView extends AbstractBlockView {
+
+  /**
+   * Initializes a new "is" text view block.
+   *
+   * @param imgPath path to image that will initialize the view block.
+   */
+  public IsTextBlockView(String imgPath) {
+    initializeBlock(imgPath);
+  }
+}
