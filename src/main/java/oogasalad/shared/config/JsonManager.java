74edc1f87c965fefc4be2ec4ede05762f6170624
Jonathan Esponda <je161@duke.edu>
2024-04-02 17:08:36 -0400

    [JsonManager Class] Changed maven dependency to gson since org.json.simple had a security vulnerability and updated usage of library

--- src/main/java/oogasalad/shared/config/JsonManager.java
+++ src/main/java/oogasalad/shared/config/JsonManager.java
@@ -1,32 +1,32 @@
 package oogasalad.shared.config;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  * JsonManager class is responsible for allowing developers to take information from the authoring
  * environment and the game player to either save it as a JSON file or to load in JSON files.
  */
 public class JsonManager {
-  private static final JSONParser parser = new JSONParser();
+  private final Gson gson = new Gson();
 
   /**
    * loadFromFile() is responsible for loading in JSON data from a JSON file that a user selects.
    *
    * @param stage stage that JavaFX uses to display the file chooser.
    */
-  public JSONObject loadFromFile(Stage stage) throws IOException {
+  public JsonObject loadFromFile(Stage stage) throws IOException {
     File file = selectFile(stage);
     if (file != null) {
       try (FileReader reader = new FileReader(file)) {
-        return (JSONObject) parser.parse(reader);
-      } catch (ParseException e) {
+        return gson.fromJson(reader, JsonObject.class);
+      } catch (JsonSyntaxException e) {
         throw new RuntimeException(e);
       }
     }
