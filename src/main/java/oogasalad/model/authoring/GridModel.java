021ee764f17285e5658c172aa6c86d2a55bb623f
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-02 02:36:35 -0400

    [DESIGN]: added documentation for GridModel

--- src/main/java/oogasalad/model/authoring/GridModel.java
+++ src/main/java/oogasalad/model/authoring/GridModel.java
@@ -5,19 +5,33 @@ import java.util.List;
 import oogasalad.shared.observer.Observable;
 import oogasalad.shared.observer.Observer;
 
+/**
+ * GridModel holds the state of a grid of blocks.
+ */
 public class GridModel implements Observable<GridModel> {
 
   private List<Observer<GridModel>> observers;
 
+  /**
+   * GridModel constructor.
+   */
   public GridModel() {
     observers = new ArrayList<>();
   }
 
+  /**
+   * Add GridModel observer to list of observers.
+   *
+   * @param o The Observer to add to notification service.
+   */
   @Override
   public void addObserver(Observer<GridModel> o) {
     observers.add(o);
   }
 
+  /**
+   * Notify observers of an update.
+   */
   @Override
   public void notifyObserver() {
     for (Observer<GridModel> observer : observers) {
