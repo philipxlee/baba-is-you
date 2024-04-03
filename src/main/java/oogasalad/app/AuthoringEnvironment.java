c86cd70eec5de583cd461b2f4e2444cde4682c42
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-03 05:00:20 -0400

    [AUTHOR ENV CONTROLLER]: added LevelController implementation with setCell method

--- src/main/java/oogasalad/app/AuthoringEnvironment.java
+++ src/main/java/oogasalad/app/AuthoringEnvironment.java
@@ -2,7 +2,9 @@ package oogasalad.app;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import oogasalad.controller.authoring.LevelController;
 import oogasalad.controller.authoring.SceneController;
+import oogasalad.model.authoring.block.BlockTypeManager;
 
 /**
  * MainController is the entrypoint for the Authoring Environment.
@@ -18,8 +20,10 @@ public class AuthoringEnvironment extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     // initialize models
+    BlockTypeManager blockTypeManager = new BlockTypeManager("/blocktypes/blocktypes.properties");
 
     // initialize controllers
+    LevelController levelController = new LevelController(blockTypeManager);
     SceneController sceneController = new SceneController(stage);
 
     // initialize views
--- src/main/java/oogasalad/controller/authoring/LevelController.java
+++ src/main/java/oogasalad/controller/authoring/LevelController.java
@@ -1,39 +1,19 @@
 package oogasalad.controller.authoring;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import oogasalad.model.authoring.block.BlockTypeManager;
 import oogasalad.model.authoring.level.Level;
 import oogasalad.model.authoring.level.LevelMetadata;
 
 public class LevelController {
 
-  private List<Level> levels;
+  private Level currentLevel;
 
-  public LevelController() {
-    this.levels = new ArrayList<>();
+  public LevelController(BlockTypeManager blockTypeManager) {
+    LevelMetadata levelMetadata = new LevelMetadata("", "", 7, 7);
+    currentLevel = new Level(levelMetadata, blockTypeManager);
   }
 
-  public Level createLevel(String name, String description, String author, int width, int height,
-      String blockPropertiesFilePath) throws IOException {
-    LevelMetadata metadata = new LevelMetadata(name, description, width, height);
-    BlockTypeManager blockTypeManager = new BlockTypeManager(blockPropertiesFilePath);
-    Level level = new Level(metadata, blockTypeManager);
-    levels.add(level);
-    return level;
-  }
-
-  public Level loadLevel(String levelId) {
-
-    return null; // Placeholder for now, actual implementation pending
-  }
-
-  public void saveLevel(String levelId) {
-
-  }
-
-  public List<Level> listLevels() {
-    return levels;
+  public void setCell(int row, int col, String blockName) throws Exception {
+    currentLevel.setCell(row, col, blockName);
   }
 }
