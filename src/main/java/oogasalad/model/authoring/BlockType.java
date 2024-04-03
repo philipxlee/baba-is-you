14d853f1d88b990ba0fa29d934de16738c2442a2
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-03 03:15:43 -0400

    refactoring blocktype

--- src/main/java/oogasalad/model/authoring/BlockType.java
+++ src/main/java/oogasalad/model/authoring/BlockType.java
@@ -1,28 +1,10 @@
 package oogasalad.model.authoring;
 
-import oogasalad.model.gameplay.blocks.AbstractBlock;
-
 /**
  * BlockType abstraction. Represents a type as an object to specify different types of Blocks.
  *
  * @param name The name of the Block Type.
  */
 public record BlockType(String name) {
-    /**
-     * Factory method to create a block of the specified type.
-     *
-     * @return The created block.
-     */
-    public AbstractBlock createBlock() {
-        switch (name.toLowerCase()) {
-//            case "baba":
-//                return new BabaBlock();
-//            case "wall":
-//                return new WallBlock();
-            // Add cases for other block types as needed
-            // unsure if to use factory method here, but for now, placeholder
-            default:
-                throw new IllegalArgumentException("Unknown block type: " + name);
-        }
-    }
+
 }
