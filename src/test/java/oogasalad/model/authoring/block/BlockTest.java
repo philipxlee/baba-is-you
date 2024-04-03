47f2bd593495a3ae58f9b28b9bc0edde9d2e7cf4
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-03 05:51:19 -0400

    [DESIGN]: added BlockTest

--- src/test/java/oogasalad/model/authoring/block/BlockTest.java
+++ src/test/java/oogasalad/model/authoring/block/BlockTest.java
@@ -1,5 +1,36 @@
 package oogasalad.model.authoring.block;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class BlockTest {
 
+  @Test
+  public void testBlockInitialization() {
+    // Setup
+    String expectedTypeName = "TestType";
+    BlockType testType = new BlockType(expectedTypeName);
+
+    // Execution
+    Block block = new Block(testType);
+
+    // Assertions
+    assertEquals(testType, block.type(),
+        "The block type should match the type it was initialized with.");
+  }
+
+  @Test
+  public void testBlockTypeProperty() {
+    // Setup
+    String typeName = "AnotherTestType";
+    BlockType anotherTestType = new BlockType(typeName);
+
+    // Execution
+    Block block = new Block(anotherTestType);
+
+    // Assertions
+    assertEquals(typeName, block.type().name(),
+        "The block's type name should be the same as the BlockType it was initialized with.");
+  }
 }
