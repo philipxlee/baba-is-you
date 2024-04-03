e32434112d5e5b169f1af1311c7fe683987da675
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-02 20:52:22 -0400

    [AUTHOR ENV MODEL]: added BlockType

---
+++ src/main/java/oogasalad/model/authoring/BlockType.java
@@ -0,0 +1,14 @@
+package oogasalad.model.authoring;
+
+public class BlockType {
+
+  private final String name;
+
+  public BlockType(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
