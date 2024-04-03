a28f04dc969e9d94c4debb21625f3a737e67d009
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-02 21:03:57 -0400

    [DESIGN]: refactored Block to a record

--- src/main/java/oogasalad/model/authoring/Block.java
+++ src/main/java/oogasalad/model/authoring/Block.java
@@ -1,14 +1,3 @@
 package oogasalad.model.authoring;
 
-public class Block {
-
-  private BlockType type;
-
-  public Block(BlockType type) {
-    this.type = type;
-  }
-
-  public BlockType getType() {
-    return type;
-  }
-}
+public record Block(BlockType type) { }
