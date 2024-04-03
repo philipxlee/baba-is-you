5d0971dcf903f4c53389267fbd6694c454e7a8f6
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-02 23:17:06 -0400

    [DESIGN]: added documentation for Blocks and BlockType

--- src/main/java/oogasalad/model/authoring/BlockType.java
+++ src/main/java/oogasalad/model/authoring/BlockType.java
@@ -1,3 +1,10 @@
 package oogasalad.model.authoring;
 
-public record BlockType(String name) { }
+/**
+ * BlockType abstraction. Represents a type as an object to specify different types of Blocks.
+ *
+ * @param name The name of the Block Type.
+ */
+public record BlockType(String name) {
+
+}
