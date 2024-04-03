2f6a9f8930b623b7db2464077d3fbb9bc925730f
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-03 17:05:32 -0400

    [DESIGN]: removed unused imports

--- src/main/java/oogasalad/controller/authoring/LevelController.java
+++ src/main/java/oogasalad/controller/authoring/LevelController.java
@@ -10,7 +10,6 @@ import oogasalad.model.authoring.level.Level;
 import oogasalad.model.authoring.level.LevelMetadata;
 
 
-
 /**
  * LevelController handles user input to modify the current level. Provides methods to interface
  * with backend model.
