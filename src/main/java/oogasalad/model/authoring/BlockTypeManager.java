c138375f4857f968cf994b19c8aa5b2003b5cd82
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-02 22:26:51 -0400

    [DESIGN]: refactored BlockTypeManager to manage a list of BlockTypes

--- src/main/java/oogasalad/model/authoring/BlockTypeManager.java
+++ src/main/java/oogasalad/model/authoring/BlockTypeManager.java
@@ -3,13 +3,16 @@ package oogasalad.model.authoring;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * BlockTypeManager loads the block types from a properties file and returns a list of block types
+ * as strings.
+ */
 public class BlockTypeManager {
 
-  private List<String> blockTypes = new ArrayList<>();
+  private List<BlockType> blockTypes = new ArrayList<>();
 
   public BlockTypeManager(String propertiesFilePath) throws IOException {
     loadBlockTypes(propertiesFilePath);
@@ -22,16 +25,21 @@ public class BlockTypeManager {
       }
       Properties properties = new Properties();
       properties.load(input);
-      String types = properties.getProperty("block.types");
-      if (types != null && !types.isEmpty()) {
-        Collections.addAll(blockTypes, types.split(","));
+      String[] types = properties.getProperty("block.types", "").split(",");
+      for (String typeName : types) {
+        blockTypes.add(new BlockType(typeName.trim()));
       }
     } catch (IOException e) {
       throw new RuntimeException("Failed to load block types from properties file");
     }
   }
 
-  public List<String> getBlockTypes() {
-    return new ArrayList<>(blockTypes);
+  public BlockType findBlockTypeByName(String name) {
+    for (BlockType blockType : blockTypes) {
+      if (blockType.name().equalsIgnoreCase(name)) {
+        return blockType;
+      }
+    }
+    throw new RuntimeException("Block name not found: " + name);
   }
 }
