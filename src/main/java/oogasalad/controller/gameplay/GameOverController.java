afae3a49511cda90900a9612279403fd7caad83a
Philip Lee <philipleektx@gmail.com>
2024-04-07 18:52:34 -0400

    [DESIGN]: Added docstrings for GameOverController for clarity

--- src/main/java/oogasalad/controller/gameplay/GameOverController.java
+++ src/main/java/oogasalad/controller/gameplay/GameOverController.java
@@ -3,14 +3,27 @@ package oogasalad.controller.gameplay;
 import oogasalad.view.gameplay.gamestates.LoseScene;
 import oogasalad.view.gameplay.gamestates.WinScene;
 
+/**
+ * Controller that manages the game over screen.
+ */
 public class GameOverController {
 
   private final SceneController sceneController;
 
+  /**
+   * Constructor for GameOverController.
+   *
+   * @param sceneController SceneController that manages the scenes.
+   */
   public GameOverController(SceneController sceneController) {
     this.sceneController = sceneController;
   }
 
+  /**
+   * Display the game over screen.
+   *
+   * @param isWin boolean representing if the player won the game.
+   */
   public void displayGameOver(boolean isWin) {
     if (isWin) {
       sceneController.switchToScene(new WinScene());
