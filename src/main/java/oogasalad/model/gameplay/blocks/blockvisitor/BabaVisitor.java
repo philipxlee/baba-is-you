32d576df9a1e2f05b9593d44ccd4fff3ebd368bf
Philip Lee <philipleektx@gmail.com>
2024-04-08 00:58:08 -0400

    [BABA-VISITOR]: Added Baba visitor to allow blocks to become Baba

---
+++ src/main/java/oogasalad/model/gameplay/blocks/blockvisitor/BabaVisitor.java
@@ -0,0 +1,64 @@
+package oogasalad.model.gameplay.blocks.blockvisitor;
+
+import oogasalad.model.gameplay.blocks.visualblocks.BabaVisualBlock;
+import oogasalad.model.gameplay.blocks.visualblocks.EmptyVisualBlock;
+import oogasalad.model.gameplay.blocks.visualblocks.FlagVisualBlock;
+import oogasalad.model.gameplay.blocks.visualblocks.RockVisualBlock;
+import oogasalad.model.gameplay.blocks.visualblocks.WallVisualBlock;
+
+/**
+ * Visitor pattern for the baba behavior.
+ */
+public class BabaVisitor implements BlockVisitor {
+
+  /**
+   * Adds the baba behavior to the baba block.
+   *
+   * @param baba the baba block
+   */
+  @Override
+  public void visit(BabaVisualBlock baba) {
+    baba.addBehavior(new BecomesBaba());
+  }
+
+  /**
+   * Adds the baba behavior to the wall block.
+   *
+   * @param wall the wall block.
+   */
+  @Override
+  public void visit(WallVisualBlock wall) {
+    wall.addBehavior(new BecomesBaba());
+  }
+
+
+  /**
+   * Adds the baba behavior to the rock block.
+   *
+   * @param rock the rock block.
+   */
+  @Override
+  public void visit(RockVisualBlock rock) {
+    rock.addBehavior(new BecomesBaba());
+  }
+
+  /**
+   * Adds the baba behavior to the flag block.
+   *
+   * @param flag the flag block.
+   */
+  @Override
+  public void visit(FlagVisualBlock flag) {
+    flag.addBehavior(new BecomesBaba());
+  }
+
+  /**
+   * Adds the baba behavior to the empty block.
+   *
+   * @param empty the empty block.
+   */
+  @Override
+  public void visit(EmptyVisualBlock empty) {
+    empty.addBehavior(new BecomesBaba());
+  }
+}
