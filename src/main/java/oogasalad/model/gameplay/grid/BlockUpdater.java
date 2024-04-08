4d4ce0734ace99adc186d4a841e33c6f40f782a2
Philip Lee <philipleektx@gmail.com>
2024-04-08 12:20:50 -0400

    [DESIGN]: Added docstrings to block updater and encapsulated empty block checkers within BecomesWall strategy

--- src/main/java/oogasalad/model/gameplay/grid/BlockUpdater.java
+++ src/main/java/oogasalad/model/gameplay/grid/BlockUpdater.java
@@ -1,32 +1,38 @@
 package oogasalad.model.gameplay.grid;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import oogasalad.model.gameplay.blocks.AbstractBlock;
 import oogasalad.model.gameplay.factory.BlockFactory;
 
+/**
+ * This class is responsible for updating the block in the grid.
+ */
 public class BlockUpdater {
 
   private final Grid grid;
   private final BlockFactory factory;
 
+  /**
+   * Constructor for BlockUpdater.
+   *
+   * @param grid  grid to update block.
+   * @param factory factory to create block.
+   */
   public BlockUpdater(Grid grid, BlockFactory factory) {
     this.grid = grid;
     this.factory = factory;
   }
 
+  /**
+   * Update the block in the grid.
+   *
+   * @param i row index.
+   * @param j column index.
+   * @param k index of block in the list.
+   * @param newBlockType new block type.
+   */
   public void updateBlock(int i, int j, int k, String newBlockType) {
     List<AbstractBlock>[][] gameGrid = grid.getGrid();
-
-    boolean containsTextBlock = gameGrid[i][j].stream()
-        .anyMatch(AbstractBlock::isTextBlock);
-    boolean containsVisualBlock = gameGrid[i][j].stream()
-        .anyMatch(block -> !block.isTextBlock() && !block.getBlockName().equals("EmptyVisualBlock"));
-
-    if (!containsTextBlock && !containsVisualBlock) {
-      gameGrid[i][j].set(k, factory.createBlock(newBlockType));
-    }
-
+    gameGrid[i][j].set(k, factory.createBlock(newBlockType));
   }
-
 }
--- src/main/java/oogasalad/model/gameplay/strategies/BecomesWall.java
+++ src/main/java/oogasalad/model/gameplay/strategies/BecomesWall.java
@@ -1,14 +1,41 @@
 package oogasalad.model.gameplay.strategies;
 
+import java.util.List;
+import oogasalad.model.gameplay.blocks.AbstractBlock;
 import oogasalad.model.gameplay.blocks.visualblocks.AbstractVisualBlock;
 import oogasalad.model.gameplay.grid.BlockUpdater;
 import oogasalad.model.gameplay.grid.Grid;
 
+/**
+ * This class is responsible for updating the block in the grid to a wall block.
+ */
 public class BecomesWall implements Strategy {
 
+  /**
+   * Update the block in the grid to a wall block if it does not contain any text or non-empty.
+   *
+   * @param grid The grid containing the block to act upon.
+   * @param updater
+   * @param i The x-coordinate of the block to act upon.
+   * @param j The y-coordinate of the block to act upon.
+   * @param k The z-coordinate of the block to act upon.
+   */
   @Override
   public void execute(Grid grid, BlockUpdater updater, int i, int j, int k) {
-    updater.updateBlock(i, j, k, "WallVisualBlock");
+    List<AbstractBlock>[][] gameGrid = grid.getGrid();
+    boolean containsTextBlock = gameGrid[i][j]
+        .stream()
+        .anyMatch(AbstractBlock::isTextBlock);
+
+    boolean containsNonEmptyVisualBlock = gameGrid[i][j]
+        .stream()
+        .anyMatch(block ->
+            !block.isTextBlock() &&
+                !block.getBlockName().equals("EmptyVisualBlock"));
+
+    if (!containsTextBlock && !containsNonEmptyVisualBlock) {
+      updater.updateBlock(i, j, k, "WallVisualBlock");
+    }
   }
 
   @Override
