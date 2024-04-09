d5af7a94a381c765a89ca1202f21ff7434b556b2
Arnav Nayak <arnavnayak2003@gmail.com>
2024-04-09 02:09:24 -0400

    [SHARED VIEW]: added load images implementation for BlockViewFactory(unfinished)

--- src/main/java/oogasalad/shared/blockview/BlockViewFactory.java
+++ src/main/java/oogasalad/shared/blockview/BlockViewFactory.java
@@ -1,8 +1,11 @@
 package oogasalad.shared.blockview;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,14 +15,23 @@ public class BlockViewFactory {
   private final Map<String, Image> imageCache;
   private final Set<String> validBlockTypes;
 
-  public BlockViewFactory(String imagesFilePath) {
+  public BlockViewFactory(String imagesFilePath) throws Exception {
     imageCache = new HashMap<>();
     validBlockTypes = new HashSet<>();
     loadImages(imagesFilePath);
   }
 
-  private void loadImages(String imagesFilePath) {
-
+  private void loadImages(String imagesFilePath) throws Exception {
+    try (InputStream input = getClass().getResourceAsStream(imagesFilePath)) {
+      if (input == null) {
+        throw new IllegalArgumentException("Image Properties file not found: " + imagesFilePath);
+      }
+      Properties properties = new Properties();
+      properties.load(input);
+      // validBlockTypes
+    } catch (IOException e) {
+      throw new Exception("Failed to load images from properties file");
+    }
   }
 
   public ImageView createBlockView(String blockType) throws Exception {
