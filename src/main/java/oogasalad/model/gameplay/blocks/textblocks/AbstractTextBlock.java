cae452588517d12b74c0bcfa4528ec685248ee63
Philip Lee <philipleektx@gmail.com>
2024-04-09 13:19:30 -0400

    [DESIGN]: Altered block factory to instantiate text blocks, allowing deletion of multiple specific text block classes

--- src/main/java/oogasalad/model/gameplay/blocks/textblocks/AbstractTextBlock.java
+++ src/main/java/oogasalad/model/gameplay/blocks/textblocks/AbstractTextBlock.java
@@ -1,15 +1,12 @@
 package oogasalad.model.gameplay.blocks.textblocks;
 
-import java.util.List;
 import oogasalad.model.gameplay.blocks.AbstractBlock;
-import oogasalad.model.gameplay.blocks.GrammarLoader;
 
 /**
  * Represents an abstract text block within the game, providing text-specific functionality.
  */
 public abstract class AbstractTextBlock extends AbstractBlock {
 
-  private static final GrammarLoader GRAMMAR_LOADER = new GrammarLoader();
   private final String name;
 
   /**
@@ -20,15 +17,6 @@ public abstract class AbstractTextBlock extends AbstractBlock {
   public AbstractTextBlock(String name) {
     super();
     this.name = name;
-  }
-
-  /**
-   * Gets the grammar for this text block.
-   *
-   * @return The grammar for this text block.
-   */
-  public List<String> getBlockGrammar() {
-    return GRAMMAR_LOADER.getGrammarForBlock(getClass().getSimpleName());
   }
 
   /**
--- src/main/java/oogasalad/model/gameplay/blocks/textblocks/TextBlock.java
+++ src/main/java/oogasalad/model/gameplay/blocks/textblocks/TextBlock.java
@@ -1,30 +1,52 @@
 package oogasalad.model.gameplay.blocks.textblocks;
 
-import oogasalad.model.gameplay.blocks.AbstractBlock;
 import java.util.List;
+import oogasalad.model.gameplay.blocks.AbstractBlock;
+import oogasalad.model.gameplay.blocks.GrammarLoader;
 
-// Assuming TextBlock is adjusted to be more generalized
 public class TextBlock extends AbstractBlock {
-  private final String name;
-  private final List<String> grammar;
 
-  public TextBlock(String name, List<String> grammar) {
+  private static final GrammarLoader GRAMMAR_LOADER = new GrammarLoader();
+  private static final String TEXT_BLOCK_SUFFIX = "TextBlock";
+  private final String name;
+
+  /**
+   * Creates a new text block with the given type.
+   *
+   * @param type The type of the text block.
+   */
+  public TextBlock(String type) {
     super();
-    this.name = name;
-    this.grammar = grammar;
+    this.name = type + TEXT_BLOCK_SUFFIX;  // Use the type as the name
   }
 
+  /**
+   * Gets the grammar for this text block.
+   *
+   * @return The grammar for this text block.
+   */
+  @Override
+  public List<String> getBlockGrammar() {
+    return GRAMMAR_LOADER.getGrammarForBlock(this.name);
+  }
+
+  /**
+   * Indicates that this block is a text block.
+   *
+   * @return Always true for text blocks.
+   */
   @Override
   public boolean isTextBlock() {
     return true;
   }
 
+  /**
+   * Gets the name of the text block.
+   *
+   * @return The name of the text block.
+   */
   @Override
   public String getBlockName() {
-    return name;
-  }
-
-  public List<String> getBlockGrammar() {
-    return grammar;
+    return this.name;
   }
 }
