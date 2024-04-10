e887429f5c6257834b2d3e09b12c8fd7cb152561
Nikita Daga <nmd43@duke.edu>
2024-04-10 03:38:10 -0400

    screen size

--- src/main/java/oogasalad/controller/authoring/SceneController.java
+++ src/main/java/oogasalad/controller/authoring/SceneController.java
@@ -21,11 +21,11 @@ public class SceneController {
    */
   public SceneController(Stage stage) {
     this.stage = stage;
-    Screen screen = Screen.getPrimary();
-    double screenWidth = screen.getBounds().getWidth();
-    double screenHeight = screen.getBounds().getHeight();
-    stage.setWidth(0.8 * screenWidth);
-    stage.setHeight(0.8 * screenHeight);
+//    Screen screen = Screen.getPrimary();
+//    double screenWidth = screen.getBounds().getWidth();
+//    double screenHeight = screen.getBounds().getHeight();
+//    stage.setWidth(0.8 * screenWidth);
+//    stage.setHeight(0.8 * screenHeight);
     stage.setTitle("Baba is Us: Authoring Environment");
     stage.setResizable(false);
     stage.show();
