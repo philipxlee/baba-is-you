539f38489bc64b10931351c3de8a47632c02a91c
Philip Lee <philipleektx@gmail.com>
2024-04-22 20:27:47 -0400

    [BLOCK-UPDATER]: Added optional for null safety when updating blocks

--- src/main/java/oogasalad/app/Main.java
+++ src/main/java/oogasalad/app/Main.java
@@ -3,9 +3,8 @@ package oogasalad.app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import oogasalad.controller.entrypoint.EntryPointController;
-import oogasalad.shared.entrypoint.EntryPoint;
 
-public class Entrance extends Application {
+public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
