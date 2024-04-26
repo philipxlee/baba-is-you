bc60a167dae0fcf5895d6becbeecc59f14341a18
Nikita Daga <nmd43@duke.edu>
2024-04-26 13:43:40 -0400

    refactored tooltip

--- src/main/java/oogasalad/view/authoring/TooltipManager.java
+++ src/main/java/oogasalad/view/authoring/TooltipManager.java
@@ -10,21 +10,21 @@ public class TooltipManager {
       Button removeButton,
       Button gptButton,
       Button saveJsonButton, Button loadLevelButton) {
-    Tooltip categoryTooltip = new Tooltip("Select the category of elements to display");
-    Tooltip difficultyTooltip = new Tooltip("Select the difficulty level of elements");
-    Tooltip changeGridSizeTooltip = new Tooltip("Click to change the size of the grid");
-    Tooltip removeButtonTooltip = new Tooltip("Toggle remove mode to delete blocks");
-    Tooltip gptButtonTooltip = new Tooltip("Generate a level using GPT");
-    Tooltip saveJsonButtonTooltip = new Tooltip("Save the level configuration as JSON");
-    Tooltip loadLevelTooltip = new Tooltip("Load a saved level configuration");
+        Tooltip categoryTooltip = new Tooltip("Select the category of elements to display");
+        Tooltip difficultyTooltip = new Tooltip("Select the difficulty level of elements");
+        Tooltip changeGridSizeTooltip = new Tooltip("Click to change the size of the grid");
+        Tooltip removeButtonTooltip = new Tooltip("Toggle remove mode to delete blocks");
+        Tooltip gptButtonTooltip = new Tooltip("Generate a level using GPT");
+        Tooltip saveJsonButtonTooltip = new Tooltip("Save the level configuration as JSON");
+        Tooltip loadLevelTooltip = new Tooltip("Load a saved level configuration");
 
-    categoryComboBox.setTooltip(categoryTooltip);
-    difficultyComboBox.setTooltip(difficultyTooltip);
-    changeGridSizeButton.setTooltip(changeGridSizeTooltip);
-    removeButton.setTooltip(removeButtonTooltip);
-    gptButton.setTooltip(gptButtonTooltip);
-    saveJsonButton.setTooltip(saveJsonButtonTooltip);
-    loadLevelButton.setTooltip(loadLevelTooltip);
+        categoryComboBox.setTooltip(categoryTooltip);
+        difficultyComboBox.setTooltip(difficultyTooltip);
+        changeGridSizeButton.setTooltip(changeGridSizeTooltip);
+        removeButton.setTooltip(removeButtonTooltip);
+        gptButton.setTooltip(gptButtonTooltip);
+        saveJsonButton.setTooltip(saveJsonButtonTooltip);
+        loadLevelButton.setTooltip(loadLevelTooltip);
 
   }
 }
