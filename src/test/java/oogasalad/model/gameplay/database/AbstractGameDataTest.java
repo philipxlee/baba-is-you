97a6f40ba56c926898dd9e6e4871f51ca7754dd7
Philip Lee <philipleektx@gmail.com>
2024-04-26 00:36:48 -0400

    [TEST-DATABASE]: Added tests for records and abstract game data

---
+++ src/test/java/oogasalad/model/gameplay/database/AbstractGameDataTest.java
@@ -0,0 +1,48 @@
+package oogasalad.model.gameplay.database;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import oogasalad.database.gamedata.AbstractGameData;
+import org.bson.Document;
+import org.junit.Test;
+import java.util.Date;
+
+public class AbstractGameDataTest {
+
+  @Test
+  public void testGetDate() {
+    AbstractGameData data = new AbstractGameData("user1", "level1", new Date()) {
+      @Override
+      public Document toDocument() {
+        return new Document();
+      }
+    };
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+    String expectedDate = dateFormat.format(new Date());
+    assertEquals(expectedDate, data.getDate());
+  }
+
+  @Test
+  public void testGetUsername() {
+    AbstractGameData data = new AbstractGameData("user1", "level1", new Date()) {
+      @Override
+      public Document toDocument() {
+        return new Document();
+      }
+    };
+    assertEquals("user1", data.getUsername());
+  }
+
+  @Test
+  public void testGetLevelName() {
+    AbstractGameData data = new AbstractGameData("user1", "level1", new Date()) {
+      @Override
+      public Document toDocument() {
+        return new Document();
+      }
+    };
+    assertEquals("level1", data.getLevelName());
+  }
+}
+
